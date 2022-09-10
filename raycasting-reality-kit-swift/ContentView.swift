42787598d2195c59b865dffac8e5baef3c434e28
Steffan Crowley <steffancrow@gmail.com>
2022-09-10 15:34:47 -0700

    refactored to use anchorEntity

--- raycasting-reality-kit-swift/ContentView.swift
+++ raycasting-reality-kit-swift/ContentView.swift
@@ -22,17 +22,7 @@ struct ARViewContainer: UIViewRepresentable {
         arView.addGestureRecognizer(UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleTap)))
         
         context.coordinator.view = arView
-        arView.session.delegate = context.coordinator
-        
-        let anchor = AnchorEntity(plane: .horizontal)
-        
-        let box = ModelEntity(mesh: MeshResource.generateBox(size: 0.3), materials: [SimpleMaterial(color: .blue, isMetallic: true)])
-        
-        box.generateCollisionShapes(recursive: true)
-        
-        anchor.addChild(box)
-        arView.scene.anchors.append(anchor)
-        
+                
         return arView
         
     }
---
+++ raycasting-reality-kit-swift/Coordinator.swift
@@ -0,0 +1,50 @@
+//
+//  Coordinator.swift
+//  raycasting-reality-kit-swift
+//
+//  Created by steffan crowley on 9/10/22.
+//
+
+import Foundation
+import ARKit
+import RealityKit
+
+class Coordinator: NSObject {
+    
+    weak var view: ARView?
+    
+    @objc func handleTap(_ recognizer: UITapGestureRecognizer) {
+        
+        guard let view = self.view else { return }
+        
+        let tapLocation = recognizer.location(in: view)
+        
+//        if let entity = view.entity(at: tapLocation) as? ModelEntity {
+//            let material = SimpleMaterial(color: UIColor.random(), isMetallic: true)
+//            entity.model?.materials = [material]
+//        }
+        
+        let results = view.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal)
+        
+        if let result = results.first {
+        
+//            let anchor = ARAnchor(name: "Plane anchor", transform: result.worldTransform)
+//                view.session.add(anchor: anchor)
+            
+            //ARAnchor is for ARKit
+            //AnchorEntity - RealityKit framework
+            
+            let anchorEntity = AnchorEntity(raycastResult: result)
+            
+            let modelEntity = ModelEntity(mesh: MeshResource.generateBox(size: 0.1))
+            modelEntity.model?.materials = [SimpleMaterial(color: .blue, isMetallic: true)]
+            
+            anchorEntity.addChild(modelEntity)
+            
+            view.scene.addAnchor(anchorEntity)
+            
+        }
+        
+    }
+    
+}
